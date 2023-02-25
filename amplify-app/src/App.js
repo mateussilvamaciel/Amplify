fd6bc05587f9b7376129196c5c2d7e52be55f753
Mateus Silva Maciel <mateussilvamaciel@gmail.com>
2023-02-25 17:49:23 -0300

    Alterações no codigo

--- amplify-app/src/App.js
+++ amplify-app/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Authenticator } from '@aws-amplify/ui-react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ singOut, user}) => (
+        <div className="App">
+          <p>
+            Ei {user.username}, bem vindo ao Lab!
+          </p>
+          <button onClick={singOut}>Sair</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
